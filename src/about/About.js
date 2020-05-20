026b3a49ad9ab51b7b9b568182763e0203f9af30
naz_liton <nazrulliton@icloud.com>
2020-05-20 15:46:06 +0100

    buttons edited and paths created in about page

--- src/about/About.js
+++ src/about/About.js
@@ -1,48 +1,66 @@
 import React, { useState } from 'react'
-import {Button} from 'react-bootstrap';
+import {Button, Container, Row, Col} from 'react-bootstrap';
 import { ChromePicker, SwatchesPicker, PhotoshopPicker } from 'react-color';
 
 
 const About = () => {
    const [color, setColor] = useState('#fff')
+   const [buttonCol, setButtonCol] = useState('lightgrey')
    const [showColorpicker, setShowColorpicker] = useState(false)
    const [showSwatchesPicker, setShowSwatchesPicker] = useState(false)
    const [showPhotoshop, seShowPhotoshop] = useState(false)
    
     return (
-        <div className='about' style={{backgroundColor: color.hex}}>
-            <h2>Convert Colours between HEX and RBG colours.</h2>
-            <p>You can play with our converter or slider to get started in your colour journey. Before diving into the world of colours and Palate, choose a base layour or main colour. The Slider is great for those that are looking for inspiration for projects and looking to create new combinations. Your only limit is your imagination. <br/>
-            Alternatively you can mess around with our Google Colour picker and Palate to find inspirations for all of your quick comparisons or if you need a colour combination quickly.</p>
+        <>
+        <Container fluid className='about' style={{backgroundColor: color.hex}}>
+            <Row style={{marginTop: '35px'}} >
+                <Col className='aboutText'>
+                <h3>Pick-A-Color</h3>    
+                <p>This a tool to select colors and color palettes for all your UI and project needs. Select whichever color picker you require, then explore colors, the background colour will change. If you need to compare or contrast a combination of colours, selecting a colour using the Color[name] Picker will change the color of the Buttons only.</p>
+                <h3>Convert between HEX and RBG colours.</h3>
+                <p>Converting between Hexadecimal color and RGB color has never been easier, you can use our converter for this function and any converting need you require. All you need to do is enter the chosen color code/digit into the Hex or RBG input field and press convert - easy as that :)</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='aboutButtons'>
+                    <Button onClick={() => setShowColorpicker(showColorpicker => !showColorpicker)} style={{backgroundColor: buttonCol.hex}}> { showColorpicker? 'Close Color Picker' : 'Open Color Picker'}  </Button>
+                    <Button onClick={()=>seShowPhotoshop(showPhotoshop => !showPhotoshop) } style={{backgroundColor: buttonCol.hex}}> {showPhotoshop ? 'Close PhotoShop picker' : 'Open Photoshop Picker'} </Button>
+                    <Button onClick={() => setShowSwatchesPicker(showSwatchesPicker => !showSwatchesPicker)} style={{backgroundColor: buttonCol.hex}} > {showSwatchesPicker? 'Close Swatches Picker' : 'Open Swatches Picker'} </Button>
+                    <Button onClick={() => setColor('#fff')}>Resit BG Color</Button>
+                    <Button onClick={() => setButtonCol('lightGrey')}>Resit Button Color</Button>
 
-            <Button onClick={() => setShowColorpicker(showColorpicker => !showColorpicker)}> { showColorpicker? 'Close Color Picker' : 'Open Color Picker'}  </Button>
-            <Button onClick={() => setShowSwatchesPicker(showSwatchesPicker => !showSwatchesPicker)} > {showSwatchesPicker? 'Close Swatches Picker' : 'open Swatches Picker'} </Button>
-            <Button onClick={()=>seShowPhotoshop(showPhotoshop => !showPhotoshop) }> {showPhotoshop ? 'Close PhotoShop picker' : 'Open Photoshop Picker'} </Button>
-            <Button onClick={() => setColor('#fff')}>Resit Color</Button>
-            {
-                showColorpicker && 
-                <ChromePicker
-                color={color}
-                onChange={updatedColour => setColor(updatedColour)}
-                />
-            }
-            {
-                showSwatchesPicker &&  
-                <SwatchesPicker
-                color={color}
-                onChange={updatedColour => setColor(updatedColour)}
-                />
-            }
-            { showPhotoshop &&   
-            <PhotoshopPicker
-            color={color}
-            onChange={updatedColour => setColor(updatedColour)}
-            />}
-
-         
-            
-           
-        </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    
+                      {
+                    showSwatchesPicker &&  
+                    <SwatchesPicker
+                    color={color}
+                    onChange={updatedColour => setColor(updatedColour)}
+                    />
+                    }
+                </Col>
+                <Col>
+                    { showPhotoshop &&   
+                    <PhotoshopPicker
+                    color={color}
+                    onChange={updatedColour => setColor(updatedColour)}
+                    />}
+                </Col>
+                <Col>
+                {
+                    showColorpicker && 
+                    <ChromePicker
+                    color={color}
+                    onChange={updatedColour => setButtonCol(updatedColour)}
+                    />
+                    }
+                </Col>
+            </Row>
+        </Container> 
+        </>
         )
 }
 
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -7,7 +7,7 @@ const Navb = () => {
         <>
         <header>
         <Navbar fixed="top" bg="light" variant="light">
-        <Navbar.Brand href="#home">Convert-A-Colour</Navbar.Brand>
+        <Navbar.Brand href="#home">Pick-A-Colour</Navbar.Brand>
         <Nav className="ml-auto">
         <Nav.Link href="#">Home</Nav.Link>
         <Nav.Link href="#converter">Converter</Nav.Link>
